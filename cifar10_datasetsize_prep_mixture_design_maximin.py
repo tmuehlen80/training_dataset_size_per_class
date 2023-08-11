d4756ffdf356cec51a82929f8568e8a5b82ab717
Thomas Mühlenstädt <32525356+tmuehlen80@users.noreply.github.com>
2023-08-11 10:08:20 +0200

    Update cifar10_datasetsize_prep_mixture_design_maximin.py

--- cifar10_datasetsize_prep_mixture_design_maximin.py
+++ cifar10_datasetsize_prep_mixture_design_maximin.py
@@ -19,11 +19,11 @@ import torch.optim as optim
 
 from torchvision import models
 
-import pyDOE
+#import pyDOE
 
-from dexpy.samplers import uniform_simplex_sample
+#from dexpy.samplers import uniform_simplex_sample
 from scipy import spatial
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 
 
@@ -108,7 +108,7 @@ print(total_params)
 
 # do one full run on the complete dataset to see the optimal performance:
 check_epochs = np.arange(0, 200, 5)
-#check_epochs = [1, 3]
+check_epochs = [1, 3]
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
@@ -161,15 +161,17 @@ print(end - start)
 
 ### set hyper parameters for data generating algo:
 
-n_repeat = 15
+#n_repeat = 15
+n_repeat_outer = 40
 subset_sizes = np.arange(5000, 45000 + 1, 5000).tolist()
-#subset_sizes = np.arange(30000, 45000 + 1, 5000).tolist()
-
+#subset_sizes = np.arange(1000, 200 + 1, 1000).tolist()
+n_repeat_inner = 3
 # the actual big loop:
 print("starting training loop for subsets:")
 print(f"check epochs: {check_epochs}")
 print(f"subsets: {subset_sizes}")
-print(f"n_repeat: {n_repeat}")
+print(f"n_repeat_outer: {n_repeat_outer}")
+print(f"n_repeat_inner: {n_repeat_inner}")
 subsets_collected = np.empty((0, n_classes))
 accs = []
 epochs_trained = []
@@ -286,73 +288,73 @@ def create_constrained_mixture_design(d, n, n_sum, c_max, n_optim, n_batch_size)
 
 for n_max in subset_sizes:
     print("optimizing doe:")
-    doe = create_constrained_mixture_design(n_classes, n_repeat, n_max, 5000, n_optim=500000, n_batch_size=1000)
-    for k in range(n_repeat):
-        #subsets = determine_sample_distr(n_classes, n_max, 5000)
-        subsets = doe[k,:]
-        class_indices = {}
-        data_subset_dict = {}
-        label_subset_dict = {}
-        for i in range(n_classes):
-            class_indices[i] = np.random.choice(5000, int(subsets[i]), replace=False)
-            label_subset_dict[i] = label_dict[i].iloc[class_indices[i]]
-            data_subset_dict[i] = data_dict[i][class_indices[i],:,:,:]
-        # now put the subset dataset together again and shuffle:
-        label_subset = pd.Series()
-        data_subset = np.empty((0, 32, 32, 3))
-        for i in range(n_classes):
-            label_subset = label_subset.append(label_subset_dict[i])
-            data_subset = np.append(data_subset, data_subset_dict[i], axis = 0)
-        data_subset = data_subset.astype(np.uint8)
-        trainset_subset.data = data_subset
-        trainset_subset.targets = label_subset.tolist()
-        trainloader_subset = torch.utils.data.DataLoader(trainset_subset, batch_size=batch_size, shuffle=True, num_workers=2)
-        # now do the actual training:
-        net = models.resnet18() 
-        _ = net.to(device)
-        criterion = nn.CrossEntropyLoss()
-        optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
-        #optimizer = optim.Adam(net.parameters(), lr=0.1, weight_decay=1e-4)
-        lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100, 150])
-        start = time()
-        for epoch in range(max(check_epochs) + 1):  # loop over the dataset multiple times
-            running_loss = 0.0
-            #for i, data in enumerate(trainloader, 0):
-            for i, data in enumerate(trainloader_subset, 0):
-                # get the inputs; data is a list of [inputs, labels]
-                inputs, labels = data[0].to(device), data[1].to(device)
-                # zero the parameter gradients
-                optimizer.zero_grad()
-                # forward + backward + optimize
-                outputs = net(inputs)
-                loss = criterion(outputs, labels)
-                loss.backward()
-                optimizer.step()
-                # print statistics
-                running_loss += loss.item()
-            lr_scheduler.step()
-            if epoch in check_epochs:
-                end = time()
-                subsets_collected = np.append(subsets_collected, subsets.reshape(1, -1), axis=0)
-                epochs_trained.append(epoch)
-                # calculate test acc:
-                correct = 0
-                total = 0
-                # since we'rnot training, we don't need to calculate the gradients for our outputs
-                with torch.no_grad():
-                    for data in testloader:
-                        #images, labels = data
-                        images, labels = data[0].to(device), data[1].to(device)
-                        # calculate outputs by running images through the network
-                        outputs = net(images)
-                        # the class with the highest energy is what we choose as prediction
-                        _, predicted = torch.max(outputs.data, 1)
-                        total += labels.size(0)
-                        correct += (predicted == labels).sum().item()
-                print(f'n_max: {n_max}, repeat {k}, epoch: {epoch}, train_loss: {running_loss:.3f}, val_loss: {running_val_loss}, Accuracy: {100 * correct // total} %, current lr: {optimizer.param_groups[0]["lr"]}')
-                times.append(end-start)
-                start = time()
-                accs.append(correct / total)
+    doe = create_constrained_mixture_design(n_classes, n_repeat_outer, n_max, 5000, n_optim=500000, n_batch_size=1000)
+    for k_outer in range(n_repeat_outer):
+        subsets = doe[k_outer,:]
+        for k_inner in range(n_repeat_inner):
+            class_indices = {}
+            data_subset_dict = {}
+            label_subset_dict = {}
+            for i in range(n_classes):
+                class_indices[i] = np.random.choice(5000, int(subsets[i]), replace=False)
+                label_subset_dict[i] = label_dict[i].iloc[class_indices[i]]
+                data_subset_dict[i] = data_dict[i][class_indices[i],:,:,:]
+            # now put the subset dataset together again and shuffle:
+            label_subset = pd.Series()
+            data_subset = np.empty((0, 32, 32, 3))
+            for i in range(n_classes):
+                label_subset = label_subset.append(label_subset_dict[i])
+                data_subset = np.append(data_subset, data_subset_dict[i], axis = 0)
+            data_subset = data_subset.astype(np.uint8)
+            trainset_subset.data = data_subset
+            trainset_subset.targets = label_subset.tolist()
+            trainloader_subset = torch.utils.data.DataLoader(trainset_subset, batch_size=batch_size, shuffle=True, num_workers=2)
+            # now do the actual training:
+            net = models.resnet18() 
+            _ = net.to(device)
+            criterion = nn.CrossEntropyLoss()
+            optimizer = optim.SGD(net.parameters(), lr=0.1, momentum=0.9, weight_decay=1e-4)
+            #optimizer = optim.Adam(net.parameters(), lr=0.1, weight_decay=1e-4)
+            lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[100, 150])
+            start = time()
+            for epoch in range(max(check_epochs) + 1):  # loop over the dataset multiple times
+                running_loss = 0.0
+                #for i, data in enumerate(trainloader, 0):
+                for i, data in enumerate(trainloader_subset, 0):
+                    # get the inputs; data is a list of [inputs, labels]
+                    inputs, labels = data[0].to(device), data[1].to(device)
+                    # zero the parameter gradients
+                    optimizer.zero_grad()
+                    # forward + backward + optimize
+                    outputs = net(inputs)
+                    loss = criterion(outputs, labels)
+                    loss.backward()
+                    optimizer.step()
+                    # print statistics
+                    running_loss += loss.item()
+                lr_scheduler.step()
+                if epoch in check_epochs:
+                    end = time()
+                    subsets_collected = np.append(subsets_collected, subsets.reshape(1, -1), axis=0)
+                    epochs_trained.append(epoch)
+                    # calculate test acc:
+                    correct = 0
+                    total = 0
+                    # since we'rnot training, we don't need to calculate the gradients for our outputs
+                    with torch.no_grad():
+                        for data in testloader:
+                            #images, labels = data
+                            images, labels = data[0].to(device), data[1].to(device)
+                            # calculate outputs by running images through the network
+                            outputs = net(images)
+                            # the class with the highest energy is what we choose as prediction
+                            _, predicted = torch.max(outputs.data, 1)
+                            total += labels.size(0)
+                            correct += (predicted == labels).sum().item()
+                    print(f'n_max: {n_max}, repeat_outer {k_outer}, repeat_inner {k_inner}, epoch: {epoch}, train_loss: {running_loss:.3f}, val_loss: {running_val_loss}, Accuracy: {100 * correct // total} %, current lr: {optimizer.param_groups[0]["lr"]}')
+                    times.append(end-start)
+                    start = time()
+                    accs.append(correct / total)
     print("writting results to file.")
     results = pd.DataFrame()
     results["accs"] = accs
@@ -360,7 +362,7 @@ for n_max in subset_sizes:
     for i, c in enumerate(classes):
         results[c] = subsets_collected[:, i]
     results["epochs_trained"] = epochs_trained
-    results.to_csv(f"Cifar10_acc_subsets_thomas_batch_size_512_mixture_design_maximin_subsetsize_{n_max}_val_20230708.csv", index=False)
+    results.to_csv(f"Cifar10_acc_subsets_thomas_batch_size_512_mixture_design_maximin_subsetsize_{n_max}_20230811_tmp.csv", index=False)
 
 
 
@@ -371,4 +373,4 @@ results["training_times"] = times
 for i, c in enumerate(classes):
     results[c] = subsets_collected[:, i]
 results["epochs_trained"] = epochs_trained
-results.to_csv("Cifar10_acc_subsets_thomas_batch_size_512_mixture_design_maximin_val_20230708.csv", index=False)
+results.to_csv("Cifar10_acc_subsets_thomas_batch_size_512_mixture_design_maximin_20230811_tmp.csv", index=False)
